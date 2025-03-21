1368a55df233fc258b6bd5fa6b94973c39fc42d5
trendyllama <agog.pretzel-0o@icloud.com>
2025-03-21 23:12:42 +0000

    Add dsa package and integrate addition function in main

--- dsa/src/lib.rs
+++ dsa/src/lib.rs
@@ -1,9 +1,10 @@
 
 
-
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
+
+
 
 #[cfg(test)]
 mod tests {
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-
-
 fn main() {
+    let result = dsa::add(2, 3);
+    println!("The sum of 2 and 3 is: {}", result);
     println!("Hello, world!");
 }
